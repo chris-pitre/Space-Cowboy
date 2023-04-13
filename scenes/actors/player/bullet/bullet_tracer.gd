55af2d2a0bf5fef17a68a54ad2289b972df56858
eggsandwich-dev <cpitre1@uncc.edu>
2023-04-13 01:12:37 -0400

    the viewport update

--- scenes/actors/player/bullet/bullet_tracer.gd
+++ scenes/actors/player/bullet/bullet_tracer.gd
@@ -4,8 +4,8 @@ var origin: Vector2
 var end: Vector2
 var new_end: Vector2
 	
-func _physics_process(delta):
-	origin = origin.move_toward(end, 50)
-	new_end = new_end.move_toward(end, 80)
+func _process(delta):
+	origin = origin.move_toward(end, 20)
+	new_end = new_end.move_toward(end, 50)
 	set_point_position(0, origin)
 	set_point_position(1, new_end)
